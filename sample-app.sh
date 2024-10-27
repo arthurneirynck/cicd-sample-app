0e8689d60f904f808fdb3c9dc50b6c506dee656f
Arthur Neirynck <arthur.neirynck@student.hogent.be>
2024-10-27 16:57:14 +0100

    Troubleshotting Jenkins

--- sample-app.sh
+++ sample-app.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 
+# Check if tempdir exists; if not, create it.
 if [ ! -d "tempdir" ]; then
     mkdir tempdir
 fi
 
-mkdir tempdir/templates
-mkdir tempdir/static
+# Check if tempdir/templates exists; if not, create it.
+if [ ! -d "tempdir/templates" ]; then
+    mkdir -p tempdir/templates
+fi
 
+# Check if tempdir/static exists; if not, create it.
+if [ ! -d "tempdir/static" ]; then
+    mkdir -p tempdir/static
+fi
+
+# Copy application files into the tempdir
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
+# Create the Dockerfile
 cat > tempdir/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
@@ -22,7 +32,8 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
 
+# Navigate into tempdir and build the Docker image
 cd tempdir || exit
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
